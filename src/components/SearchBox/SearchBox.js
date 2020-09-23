767a762a0d32376c4a77955ff151bb2b9c1ba230
ChongLynk <tronglinhmta0611@gmail.com>
2020-09-23 05:10:07 -0400

    Input bien mat icon khi focus

---
+++ src/components/SearchBox/SearchBox.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import classNames from 'classnames';
+import './style.css';
+
+class SearchBox extends Component{
+    constructor(){
+        super();
+        this.state = {
+            isFocused:false
+        };
+        
+    }
+    focusInput(){
+        this.setState({isFocused:true});
+    }
+    render(){
+        return(
+            <div className="SearchBox">
+                <input placeholder="type something to search..." onFocus={this.focusInput.bind(this)}></input>
+                <div className={classNames("container-icon",{active: this.state.isFocused})}>
+                    <i class="fas fa-search"></i>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default SearchBox;
